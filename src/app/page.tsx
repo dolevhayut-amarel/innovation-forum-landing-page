f772c42d57753d748137ae253ff174ece7975aeb
Dolev Hayut <dolev.h@amarel.net>
2026-05-10 10:24:11 +0300

    checkpoint: claude 2026-05-10 10:24 [auto]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { StickyHeader } from "@/components/StickyHeader";
 import { HeroSection } from "@/components/HeroSection";
-import { HeroCtaSection } from "@/components/HeroCtaSection";
 import { ClientsBar } from "@/components/ClientsBar";
 import { BrandStatement } from "@/components/BrandStatement";
 import { LeadsToSuccess } from "@/components/LeadsToSuccess";
@@ -17,8 +16,7 @@ export default function Home() {
       <StickyHeader />
       <main dir="rtl" className="w-full overflow-x-hidden">
       <HeroSection />
-      <HeroCtaSection />
-      <ClientsBar />
+<ClientsBar />
       <BrandStatement />
       <LeadsToSuccess />
       <WhyStudySection />
